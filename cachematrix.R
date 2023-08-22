d7edaceaea80f5170fd2b41437f113b03ad601ff
DrisyaKrishnan <134757120+DrisyaKrishnan@users.noreply.github.com>
2023-08-22 13:22:27 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,22 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+##makeCacheMatrix includes set,get,setinv,getinv
+##library(MASS) is used to calculate inverse of both squared and non squared matrices
+library(MASS)
 makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  inv<-NULL                       #initializing inverse as NULL
+  set<-function(y){
+                  x<<-y
+                  inv<<-NULL
+                  }
+ get<-function()x                 #the function to get matrix x
+ setinv<-function(inverse)inv<<-inverse 
+ getinv<-function(){
+                  inver<-ginv(x)
+                  inver%*%x                 #function to get inverse of matrix
+                  } 
+ list(set = set, get = get,
+      setinv = setinv,
+      getinv = getinv)
+ 
